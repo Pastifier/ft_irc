154a546072260b840cacde4d31f2f569b87a86d7
Emran BinJamaan <therealemran.design@gmail.com>
2025-03-15 14:42:59 +0100

    refactor: is_valid_port() function.

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,19 @@
-#include "../include/Server.hpp"
+#include "Server.hpp"
+#include <sstream>
 
 int is_valid_port(std::string input) {
-	try {
-		size_t idx = 0;
-		int number = stoi(input, &idx);
-		if (idx != input.size())
-			throw std::invalid_argument("Error: The value of the port is not integer.\n");
-		if (number < 1 || number > 65535) {
-			throw std::out_of_range("Error: The port number is out of range (1-65535).");
-		}
-		return number;
-	} catch (const std::invalid_argument& e) {
-		std::cerr << "Error: The value of the port is not integer." << std::endl;
-	} catch (const std::out_of_range& e) {
-		std::cout << "Error: The port number is out of range (1-65535)." << std::endl;
+	std::istringstream iss(input);
+	int portno;
+
+	iss >> portno;
+	if (iss.fail() || !iss.eof()) {
+		std::cerr << "Error: invalid port number. Please make sure that the input is an integer in the range (1->65535)." << std::endl;
+		return -1;
+	} else if (portno < 1 || portno > 65535) {
+		std::cerr << "Error: invalid port number. Please make sure that the input is an integer in the range (1->65535)." << std::endl;
+		return -1;
 	}
-	return -1;
+	return portno;
 }
 
 int main(int argc, char *argv[]) {
@@ -23,7 +21,7 @@ int main(int argc, char *argv[]) {
 		std::cerr << "Usage: ./ircserv port password" << std::endl;
 		return 1;
 	}
-	std::string input = argv[1];
+	std::string input(argv[1]);
 	int port = is_valid_port(input);
 	if (port == -1)
 		return 1;
