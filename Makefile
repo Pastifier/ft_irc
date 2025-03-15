a46c64e042148f00739808659df23711e24118ba
surajbartoula <suraj_bartoula@yahoo.com>
2025-03-15 15:30:48 +0400

    Checking for valid port between 1-65535

--- Makefile
+++ Makefile
@@ -1,3 +1,14 @@
+NAME = ircserv
+
+SRC = src/main.cpp
+
+OBJS = $(SRC:.cpp=.o)
+
+CC = c++
+
+CFLAGS = -Wall -Wextra -Werror -std=c++98
+
+all: $(NAME)
 
 irssi:
 	docker run -it --name irssi -e TERM -u $(id -u):$(id -g) \
@@ -5,4 +16,20 @@ irssi:
 	   -v ${HOME}/.irssi:/home/user/.irssi:ro \
 	irssi
 	docker rm -f irssi 2>/dev/null
+
+.cpp.o:
+	$(CC) $(CFLAGS) -c $< -o $@
+
+$(NAME): $(OBJS)
+	$(CC) $(OBJS) $(CFLAGS) -o $(NAME)
+
+clean:
+	@$(RM) $(OBJS)
+
+fclean: clean
+	@$(RM) $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
 #include "../include/Server.hpp"
 
+int is_valid_port(std::string input) {
+	try {
+		size_t idx = 0;
+		int number = stoi(input, &idx);
+		if (idx != input.size())
+			throw std::invalid_argument("Error: The value of the port is not integer.\n");
+		if (number < 1 || number > 65535) {
+			throw std::out_of_range("Error: The port number is out of range (1-65535).");
+		}
+		return number;
+	} catch (const std::invalid_argument& e) {
+		std::cerr << "Error: The value of the port is not integer." << std::endl;
+	} catch (const std::out_of_range& e) {
+		std::cout << "Error: The port number is out of range (1-65535)." << std::endl;
+	}
+	return -1;
+}
+
 int main(int argc, char *argv[]) {
 	if (argc != 3) {
 		std::cerr << "Usage: ./ircserv port password" << std::endl;
 		return 1;
 	}
+	std::string input = argv[1];
+	int port = is_valid_port(input);
+	if (port == -1)
+		return 1;
 	return 0;
 }
